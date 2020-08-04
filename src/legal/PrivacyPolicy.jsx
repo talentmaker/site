0860c92fae3ad4e12c75c25e0893d503c0255241
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2020-08-04 09:56:51 -0400

    add privacy policy
    Signed-off-by: Luke-zhang-04 <luke.zhang2004dev@gmail.com>

---
+++ src/legal/PrivacyPolicy.jsx
@@ -0,0 +1,36 @@
+/**
+ * Talentmaker website
+ * 
+ * Copyright (C) 2020 Luke Zhang - Luke-zhang-04.github.io
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import React from "react"
+
+const PrivacyPolicy = () => (
+    <div className="container">
+        <h2>Privacy Policy</h2>
+        <p>Your privacy is important to us. It is Talentmaker&apos;s policy to respect your privacy regarding any information we may collect from you across our website, <a href="https://www.google.com/">https://www.google.com/</a>, and other sites we own and operate.</p>
+        <p>We only ask for personal information when we truly need it to provide a service to you. We collect it by fair and lawful means, with your knowledge and consent. We also let you know why we’re collecting it and how it will be used.</p>
+        <p>We only retain collected information for as long as necessary to provide you with your requested service. What data we store, we’ll protect within commercially acceptable means to prevent loss and theft, as well as unauthorised access, disclosure, copying, use or modification.</p>
+        <p>We don’t share any personally identifying information publicly or with third-parties, except when required to by law.</p>
+        <p>Our website may link to external sites that are not operated by us. Please be aware that we have no control over the content and practices of these sites, and cannot accept responsibility or liability for their respective privacy policies.</p>
+        <p>You are free to refuse our request for your personal information, with the understanding that we may be unable to provide you with some of your desired services.</p>
+        <p>Your continued use of our website will be regarded as acceptance of our practices around privacy and personal information. If you have any questions about how we handle user data and personal information, feel free to contact us.</p>
+        <p>This policy is effective as of 3 August 2020.</p>
+    </div>
+)
+
+export default PrivacyPolicy
