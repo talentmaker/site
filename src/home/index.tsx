4a10fa84419ae4967a8f22aa8cb7bc7bbbedff7e
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2020-09-05 22:33:27 -0400

    try to fix for CI
    Signed-off-by: Luke-zhang-04 <luke.zhang2004dev@gmail.com>

--- src/home/index.tsx
+++ src/home/index.tsx
@@ -18,7 +18,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-// import "./home.scss"
+import "./home.scss"
 import React from "react"
 
 interface HomeState {
