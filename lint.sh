e895c0466f7c741cb1904ba499f168c3b24ba2d4
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2020-09-19 21:55:40 -0400

    use eslint_d for faster linting and formatting
    Signed-off-by: Luke-zhang-04 <luke.zhang2004dev@gmail.com>

--- lint.sh
+++ lint.sh
@@ -2,7 +2,7 @@
 
 if [ "$1" == "--fix" ]||[ "$1" == "-f" ]; then
     echo "Begin lint"
-    ./node_modules/.bin/esprint --fix &
+    ./node_modules/.bin/eslint_d ./*/**/*."{js,jsx,ts,tsx}" --fix &
     ./node_modules/.bin/stylelint ./*/**/*."{scss,css}" --fix &
 
     echo "Waiting for lint to finish..."
@@ -11,7 +11,7 @@ if [ "$1" == "--fix" ]||[ "$1" == "-f" ]; then
     echo "Completed lint"
 else
     echo "Begin lint"
-    ./node_modules/.bin/esprint &
+    ./node_modules/.bin/eslint_d ./*/**/*."{js,jsx,ts,tsx}" &
     ./node_modules/.bin/stylelint ./*/**/*."{scss,css}" &
 
     echo "Waiting for lint to finish..."
