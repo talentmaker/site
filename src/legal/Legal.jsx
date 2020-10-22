a61768485e547345ed0bb180aa324fd4e0b83f13
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2020-10-21 20:45:13 -0400

    change license to BSD-3
    Signed-off-by: Luke-zhang-04 <luke.zhang2004dev@gmail.com>

--- src/legal/Legal.jsx
+++ src/legal/Legal.jsx
@@ -1,32 +1,19 @@
 /**
  * Talentmaker website
- * 
+ *
  * @copyright (C) 2020 Luke Zhang, Ethan Lim
- * @author Luke Zhang - luke-zhang-04.github.io 
- *
- * @license GPL-3.0
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published
- * by the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * @license BSD-3-Clause
+ * @author Luke Zhang - luke-zhang-04.github.io
  */
-import Gpl from "./Gpl"
+import Bsd3 from "./Bsd3"
 import React from "react"
 
 const Legal = () => (
     <div className="container">
         <h1>Talentmaker Website Terms of Use</h1>
         <p>Copyright (C) 2020 <a href="https://luke-zhang-04.github.io/">Luke Zhang</a>, <a href="https://github.com/ethanlim04">Ethan Lim</a>, and Talentmaker</p>
-        <p>This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License (below) for more details.</p>
-        <Gpl/>
+        <p>This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the BSD 3 Clause License (below) for more details.</p>
+        <Bsd3/>
         <p>END OF TERMS AND CONDITIONS</p>
     </div>
 )
