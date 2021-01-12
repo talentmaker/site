47407af991bb7ebb118ae3da3b2acf8ad441b393
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2021-01-11 23:29:55 -0500

    improve cache system
    Signed-off-by: Luke-zhang-04 <luke.zhang2004dev@gmail.com>

--- src/cache.ts
+++ src/cache.ts
@@ -13,52 +13,144 @@
 import lzString from "lz-string"
 
 /**
+ * An entry into localstorage
+ */
+type CacheEntry = {
+    lastUsed: number,
+    data: string,
+}
+
+/**
+ * Typegaurd to make sure obj is a cache entry
+ */
+const isCacheEntry = (obj: unknown): obj is CacheEntry => (
+        typeof obj === "object" &&
+        typeof (obj as {[key: string]: unknown}).lastUsed === "number" &&
+        typeof (obj as {[key: string]: unknown}).data === "string"
+    ),
+
+    /**
+     * Gets all localstorage cache entries, and removes the oldest ones
+     * Also removed problematic cache entries that cause errors
+     */
+    cleanCache = async (): Promise<void> => (
+        await Promise.resolve().then(() => {
+            const entries: [lastUsed: number, key: string][] = []
+
+            for (const key of Object.keys(localStorage)) {
+                try {
+                    const item = localStorage.getItem(key)
+
+                    if (item === null) {
+                        // eslint-disable-next-line
+                        continue
+                    }
+
+                    const parsedItem: unknown = JSON.parse(item)
+
+                    if (isCacheEntry(parsedItem)) {
+                        entries.push([parsedItem.lastUsed, key])
+                    } else {
+                        // eslint-disable-next-line
+                        throw undefined
+                    }
+                } catch {
+                    if ((/^talentmakerCache.*/u).test(key)) {
+                        localStorage.removeItem(key) // Remove error-causing keys
+                    }
+                }
+            }
+
+            const sortedEntries = entries
+                .sort((first, second) => second[0] - first[0]) // Sort from oldest to newest
+                .slice(entries.length - entries.length / 8) // Include 1/8 of the storage
+
+            for (const [_, key] of sortedEntries) {
+                localStorage.removeItem(key) // Remove storage items
+            }
+        })
+    ),
+
+    /**
+     * If localstorage is enabled
+     */
+    haslocalStorage = ((): boolean => {
+        if (localStorage === undefined) {
+            return false
+        }
+
+        try {
+            localStorage.setItem("LSTest", "TEST")
+
+            return localStorage.getItem("LSTest") === "TEST"
+        } catch {
+            return false
+        }
+    })()
+
+/**
  * Writes to localstorage with key
  * @param key - key to write data
  * @param data - data to write
  */
-export const writeCache = (key: string, data: unknown): Promise<void> => (
-    new Promise((resolve) => {
+export const writeCache = async (key: string, data: unknown): Promise<void> => {
+    if (!haslocalStorage) {
+        return
+    }
+
+    return await Promise.resolve().then(() => {
         try {
             localStorage.setItem(
                 key,
-                lzString.compressToUTF16(JSON.stringify(data)),
+                JSON.stringify({
+                    lastUsed: Date.now(),
+                    data: lzString.compressToUTF16(JSON.stringify(data)),
+                }),
             )
         } catch {
-            return resolve()
+            cleanCache()
         }
-
-        return resolve()
     })
-)
+}
 
 /**
  * Reads from localstorage and tries to parse the data
  * @param key - key for data
  */
-export const readCache = (key: string): Promise<unknown> => (
-    new Promise((resolve) => {
-        const data = localStorage.getItem(key)
+export const readCache = async (key: string): Promise<unknown> => {
+    if (!haslocalStorage) {
+        return
+    }
 
-        if (data === null) {
-            return resolve(null)
+    return await Promise.resolve().then(() => {
+        const cacheEntry = localStorage.getItem(key)
+
+        if (cacheEntry === null) {
+            return null
         }
 
         try {
-            const decompressedData = lzString.decompressFromUTF16(data)
+            const parsedCacheEntry: unknown = JSON.parse(cacheEntry)
 
-            if (decompressedData) {
-                const parsedData = JSON.parse(decompressedData)
-
-                return resolve(parsedData)
+            if (!isCacheEntry(parsedCacheEntry)) {
+                return null
             }
 
-            return resolve(null)
+            const decompressedData =
+                lzString.decompressFromUTF16(parsedCacheEntry.data)
+
+            if (decompressedData) {
+                const parsedData: unknown = JSON.parse(decompressedData)
+
+                return parsedData
+            }
+
+            return null
         } catch {
-            return resolve(null)
+            return null
         }
     })
-)
+}
 
 export default {
     read: readCache,
