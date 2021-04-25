ea0a303677082b25d26e000bae093423bf382505
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2021-04-25 12:19:31 -0400

    fix linter

--- lint.sh
+++ lint.sh
@@ -2,8 +2,8 @@
 
 if [ "$1" == "--fix" ]||[ "$1" == "-f" ]; then
     echo "Begin lint"
-    ./node_modules/.bin/eslint_d ./*/**/*."{js,jsx,ts,tsx}" --fix &
-    ./node_modules/.bin/stylelint ./*/**/*."{scss,css}" --fix &
+    ./node_modules/.bin/eslint_d ./src/"{*,**/*}"."{js,jsx,ts,tsx}" --max-warnings=0 --fix &
+    ./node_modules/.bin/stylelint ./src/"{*,**/*}"."{scss,css}" --max-warnings=0 --fix &
 
     echo "Waiting for lint to finish..."
     wait
@@ -11,14 +11,14 @@ if [ "$1" == "--fix" ]||[ "$1" == "-f" ]; then
     echo "Completed lint"
 elif [ "$1" == "-CI" ]; then
     echo "Begin lint"
-    ./node_modules/.bin/eslint_d ./*/**/*."{js,jsx,ts,tsx}"
-    ./node_modules/.bin/stylelint ./*/**/*."{scss,css}"
+    ./node_modules/.bin/eslint ./src/"{*,**/*}"."{js,jsx,ts,tsx}" --max-warnings=0
+    ./node_modules/.bin/stylelint ./src/"{*,**/*}"."{scss,css}" --max-warnings=0
 
     echo "Completed lint"
 else
     echo "Begin lint"
-    ./node_modules/.bin/eslint_d ./*/**/*."{js,jsx,ts,tsx}" &
-    ./node_modules/.bin/stylelint ./*/**/*."{scss,css}" &
+    ./node_modules/.bin/eslint_d ./src/"{*,**/*}"."{js,jsx,ts,tsx}" --max-warnings=0 &
+    ./node_modules/.bin/stylelint ./src/"{*,**/*}"."{scss,css}" --max-warnings=0 &
 
     echo "Waiting for lint to finish..."
     wait
