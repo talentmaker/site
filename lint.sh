546e23cd7d6310e16d54875c5b309f6a9868f612
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2021-06-06 16:50:20 -0400

    lint: use cache and --ext

--- lint.sh
+++ lint.sh
@@ -2,8 +2,8 @@
 
 if [ "$1" == "--fix" ]||[ "$1" == "-f" ]; then
     echo "Begin lint"
-    ./node_modules/.bin/eslint_d ./src/"{*,**/*}"."{js,jsx,ts,tsx}" --max-warnings=0 --fix &
-    ./node_modules/.bin/stylelint ./src/"{*,**/*}"."{scss,css}" --max-warnings=0 --fix &
+    ./node_modules/.bin/eslint_d --ext .js,.jsx,.ts,.tsx --cache --max-warnings=0 --fix src &
+    ./node_modules/.bin/stylelint ./src/"{*,**/*}"."{scss,css}" --cache --max-warnings=0 --fix &
 
     echo "Waiting for lint to finish..."
     wait
@@ -11,14 +11,14 @@ if [ "$1" == "--fix" ]||[ "$1" == "-f" ]; then
     echo "Completed lint"
 elif [ "$1" == "-CI" ]; then
     echo "Begin lint"
-    ./node_modules/.bin/eslint ./src/"{*,**/*}"."{js,jsx,ts,tsx}" --max-warnings=0
+    ./node_modules/.bin/eslint --ext .js,.jsx,.ts,.tsx --max-warnings=0 src
     ./node_modules/.bin/stylelint ./src/"{*,**/*}"."{scss,css}" --max-warnings=0
 
     echo "Completed lint"
 else
     echo "Begin lint"
-    ./node_modules/.bin/eslint_d ./src/"{*,**/*}"."{js,jsx,ts,tsx}" --max-warnings=0 &
-    ./node_modules/.bin/stylelint ./src/"{*,**/*}"."{scss,css}" --max-warnings=0 &
+    ./node_modules/.bin/eslint_d --ext .js,.jsx,.ts,.tsx --cache --max-warnings=0 src &
+    ./node_modules/.bin/stylelint ./src/"{*,**/*}"."{scss,css}" --cache --max-warnings=0 &
 
     echo "Waiting for lint to finish..."
     wait
