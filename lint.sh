0406b7a8dae184c0e925db14bfa494b69c76c9fb
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2021-06-06 21:01:37 -0400

    repalce eslint_d with --cache option

--- lint.sh
+++ lint.sh
@@ -2,7 +2,7 @@
 
 if [ "$1" == "--fix" ]||[ "$1" == "-f" ]; then
     echo "Begin lint"
-    ./node_modules/.bin/eslint_d --ext .js,.jsx,.ts,.tsx --cache --max-warnings=0 --fix src &
+    ./node_modules/.bin/eslint --ext .js,.jsx,.ts,.tsx --cache --max-warnings=0 --fix src &
     ./node_modules/.bin/stylelint ./src/"{*,**/*}"."{scss,css}" --cache --max-warnings=0 --fix &
 
     echo "Waiting for lint to finish..."
@@ -17,7 +17,7 @@ elif [ "$1" == "-CI" ]; then
     echo "Completed lint"
 else
     echo "Begin lint"
-    ./node_modules/.bin/eslint_d --ext .js,.jsx,.ts,.tsx --cache --max-warnings=0 src &
+    ./node_modules/.bin/eslint --ext .js,.jsx,.ts,.tsx --cache --max-warnings=0 src &
     ./node_modules/.bin/stylelint ./src/"{*,**/*}"."{scss,css}" --cache --max-warnings=0 &
 
     echo "Waiting for lint to finish..."
