873c0d0a58b9c70827e369d1c94f966c1e2617c5
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2021-06-10 15:30:20 -0400

    run prettier

--- src/components/pages/privacyPolicy/index.jsx
+++ src/components/pages/privacyPolicy/index.jsx
@@ -16,8 +16,8 @@ export const PrivacyPolicy = () => (
         <p>
             Your privacy is important to us. It is Talentmaker&apos;s policy to respect your
             privacy regarding any information we may collect from you across our website,{" "}
-            <a href="https://talentmaker.ca">https://talentmaker.ca</a>, and other sites we own
-            and operate.
+            <a href="https://talentmaker.ca">https://talentmaker.ca</a>, and other sites we own and
+            operate.
         </p>
         <p>
             We only ask for personal information when we truly need it to provide a service to you.
