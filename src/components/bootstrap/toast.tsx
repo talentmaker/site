52fbeec39a35f6343ffe7dbb7cf5e3d529272c1c
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2021-06-14 00:27:31 -0400

    chore: remove unused export
    Signed-off-by: Luke-zhang-04 <luke.zhang2004dev@gmail.com>

--- src/components/bootstrap/toast.tsx
+++ src/components/bootstrap/toast.tsx
@@ -65,11 +65,6 @@ export interface Props {
     onClick?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void
 }
 
-interface State {
-    shouldShow: boolean
-    time: number | string
-}
-
 const getTimeFromSeconds = (time: number): string => {
     const date = DatePlus.secondsToHours(time)
 
@@ -144,91 +139,6 @@ export const Toast: React.FC<Props> = ({
             <BsToast.Body>{children}</BsToast.Body>
         </BsToast>
     ) : null
-}
-
-export class Toast2 extends React.PureComponent<Props, State> {
-    private static _getTimeFromSeconds = (time: number): string => {
-        const date = DatePlus.secondsToHours(time)
-
-        if (date.hours > 0) {
-            return `${date.hours} hour${date.hours > 1 ? "s" : ""}, ${date.minutes} minute${
-                date.minutes > 1 ? "s" : ""
-            } ago`
-        } else if (date.minutes > 0) {
-            return `${date.minutes} minute${date.minutes > 1 ? "s" : ""}, ${date.seconds} second${
-                date.seconds > 1 ? "s" : ""
-            } ago`
-        }
-
-        return `${date.seconds} second${date.seconds > 1 ? "s" : ""} ago`
-    }
-
-    public constructor(props: Props) {
-        super(props)
-
-        this.state = {
-            shouldShow: true,
-            time: props.time ?? 0,
-        }
-    }
-
-    public componentDidMount = (): void => {
-        if (typeof this.state.time === "number") {
-            const interval = setInterval((): void => {
-                const {time} = this.state
-
-                if (typeof time === "number") {
-                    this.setState({time: time + 1})
-                }
-            }, Time.Second)
-
-            this._intervalId = Number(`${interval}`)
-        }
-    }
-
-    public componentWillUnmount = (): void => {
-        clearInterval(this._intervalId)
-    }
-
-    private _intervalId?: number
-
-    /**
-     * Bootstrap toast
-     */
-    private _toast = (): JSX.Element => (
-        <div
-            className={`toast ${this.state.shouldShow ? "show" : "hide"} bg-lighter toast-fixed`}
-            aria-live={this.props.assertive ? "assertive" : "polite"}
-            aria-atomic="true"
-            ref={this.props.reference}
-        >
-            <div className="toast-header text-dark bg-light">
-                <span className={`material-icons me-2 ${this.props.iconClassName ?? ""}`}>
-                    {this.props.icon ?? "error"}
-                </span>
-                <strong className="me-auto">{this.props.title}</strong>
-                <small className="text-muted">
-                    {typeof this.state.time === "number"
-                        ? Toast2._getTimeFromSeconds(this.state.time)
-                        : this.state.time}
-                </small>
-                <button
-                    type="button"
-                    className="btn-close text-lighter"
-                    data-bs-dismiss="toast"
-                    aria-label="Close"
-                    onClick={(event): void => {
-                        this.setState({shouldShow: false})
-
-                        this.props.onClick?.(event)
-                    }}
-                ></button>
-            </div>
-            <div className="toast-body">{this.props.children}</div>
-        </div>
-    )
-
-    public render = (): JSX.Element => (this.state.shouldShow ? this._toast() : <></>)
 }
 
 export default Toast
