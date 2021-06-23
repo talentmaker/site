2fb0585e8950bbbc3a117d584da1ed5644f8b001
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2021-06-23 15:36:41 -0400

    CI: add browserCheck

---
+++ browserCheck.js
@@ -0,0 +1,14 @@
+#!/bin/node
+
+const browserslist = require("browserslist")
+
+const threshold = Number(process.env.THRESHOLD ?? 90)
+const coverage = browserslist.coverage(browserslist()) // Check coverage from .browserslistrc
+
+if (coverage >= threshold) {
+    console.log(`Coverage is ${coverage}%, passing`)
+} else {
+    throw new Error(
+        `Coverage is ${coverage}%, which does not meet the >= ${threshold}% requirement.`,
+    )
+}
