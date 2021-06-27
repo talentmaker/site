085eb7a741bb9bb4dd3176c3e00ad3bd57d68cd4
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2021-06-26 20:19:38 -0400

    update browserCheck

--- browserCheck.js
+++ browserCheck.js
@@ -5,10 +5,14 @@ const browserslist = require("browserslist")
 const threshold = Number(process.env.THRESHOLD ?? 90)
 const coverage = browserslist.coverage(browserslist()) // Check coverage from .browserslistrc
 
+class CoverageError extends Error {
+    name = "CoverageError"
+}
+
 if (coverage >= threshold) {
-    console.log(`Coverage is ${coverage}%, passing`)
+    console.log(`Coverage is ${coverage}%, which meets the >= ${threshold}% requirement; passing.`)
 } else {
-    throw new Error(
-        `Coverage is ${coverage}%, which does not meet the >= ${threshold}% requirement.`,
+    throw new CoverageError(
+        `coverage is ${coverage}%, which does not meet the >= ${threshold}% requirement; failing.`,
     )
 }
