05749a58906f66df215f3148152283bd39dcc2bc
Luke-zhang-04 <Luke_zhang_04@protonmail.com>
2021-12-23 22:54:50 +0000

    deps: bump stylelint to 14.2.0

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -5,40 +5,29 @@ module.exports = {
     rules: {
         indentation: null, // Work with prettier
         "declaration-colon-newline-after": null,
-        "at-rule-no-unknown": [
-            true,
-            {
-                ignoreAtRules: [
-                    "use",
-                    "forward",
-                    "import",
-                    "mixin",
-                    "include",
-                    "function",
-                    "return",
-                    "extend",
-                    "at-root",
-                    "error",
-                    "warn",
-                    "debug",
-                    "if",
-                    "each",
-                    "for",
-                    "while",
-                ],
-            },
-        ],
-        "no-eol-whitespace": [
-            true,
-            {
-                ignore: ["empty-lines"],
-            },
-        ],
-        "selector-pseudo-class-no-unknown": [
-            true,
-            {
-                ignorePseudoClasses: ["global"],
-            },
-        ],
     },
+    overrides: [
+        {
+            files: ["**/*.module.*"],
+            rules: {
+                "selector-pseudo-class-no-unknown": [
+                    true,
+                    {
+                        ignorePseudoClasses: ["global"],
+                    },
+                ],
+                "selector-class-pattern":
+                    /(^([a-z][a-z0-9]*)(-[a-z0-9]+)*$)|(^[a-z][a-zA-Z0-9]+$)/,
+            },
+        },
+        {
+            files: ["**/*.scss"],
+            extends: ["stylelint-config-standard-scss", "stylelint-prettier/recommended"],
+            rules: {
+                "scss/dollar-variable-empty-line-before": null,
+                "scss/function-quote-no-quoted-strings-inside": null,
+                "scss/at-mixin-argumentless-call-parentheses": null,
+            },
+        },
+    ],
 }
