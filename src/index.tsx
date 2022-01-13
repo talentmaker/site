ee4ae3a68c62abdfe28df81eaa0d68c900ebf8b5
Luke-zhang-04 <Luke_zhang_04@protonmail.com>
2022-01-13 16:05:02 -0500

    use AOS for homepage

--- src/index.tsx
+++ src/index.tsx
@@ -11,6 +11,9 @@
 // Load initialized stuff
 import "./init"
 
+// AOS
+import "aos/dist/aos.css"
+
 // Load Prismjs languages
 import "prismjs"
 import "prismjs/components/prism-clike"
@@ -45,6 +48,7 @@ import {NotificationContext, ThemeContext, UserContext} from "./contexts"
 import {NotificationType, Notifications} from "./components/notifications"
 import {Route, BrowserRouter as Router, Routes} from "react-router-dom"
 import {CognitoUser as User, userSchema} from "./schemas/user"
+import AOS from "aos"
 import ErrorBoundary from "./components/errorBoundary"
 import Footer from "./components/footer"
 import {Helmet} from "react-helmet"
@@ -189,6 +193,27 @@ const App: React.FC = () => {
             } else {
                 setThemeContext("light")
             }
+        })
+
+        AOS.init({
+            // Global settings:
+            disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+            startEvent: "DOMContentLoaded", // name of the event dispatched on the document, that AOS should initialize on
+            initClassName: "aos-init", // class applied after initialization
+            animatedClassName: "aos-animate", // class applied on animation
+            useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+            disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+            debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+            throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+
+            // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+            offset: 120, // offset (in px) from the original trigger point
+            delay: 0, // values from 0 to 3000, with step 50ms
+            duration: 400, // values from 0 to 3000, with step 50ms
+            easing: "ease", // default easing for AOS animations
+            once: false, // whether animation should happen only once - while scrolling down
+            mirror: false, // whether elements should animate out while scrolling past them
+            anchorPlacement: "top-bottom", // defines which position of the element regarding to window should trigger the animation
         })
     }, [])
 
